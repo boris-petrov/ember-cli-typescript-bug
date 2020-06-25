8ee8681b153db357e3abb5e44eae9a160cb0281a
Boris Petrov <boris_petrov@live.com>
2020-06-25 17:57:58 +0300

    Add simple reproduction

---
+++ app/components/foo-bar.ts
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+import { classNames } from '@ember-decorators/component';
+
+@classNames('col-lg-2', 'col-md-4', 'col-sm-6', 'col-xs-12')
+export default class extends Component {}
